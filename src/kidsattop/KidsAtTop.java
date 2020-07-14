40f2f698f3b4520b871b999a728073ec2db0846c
EDUARDO <EDUARDO@DESKTOP-9H26RGO>
2020-07-14 15:43:06 -0600

    Second Release

--- src/kidsattop/KidsAtTop.java
+++ src/kidsattop/KidsAtTop.java
@@ -10,6 +10,7 @@ public class KidsAtTop {
 
     public static void main(String[] args) {
         //Prueba
+        //prueba2
         LoginForm formulario = new LoginForm();
         formulario.setVisible(true);
     }
