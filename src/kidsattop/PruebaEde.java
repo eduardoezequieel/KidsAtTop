b82a1e16abe916a85bdf451428149d24a515c060
RamirezCell <52053847+RamirezCell@users.noreply.github.com>
2020-07-18 17:48:45 -0600

    hice una clase con mi nombre

---
+++ src/kidsattop/PruebaEde.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kidsattop;
+
+/**
+ *
+ * @author polon
+ */
+public class PruebaEde {
+    
+    //cabañas no existe
+    
+}
