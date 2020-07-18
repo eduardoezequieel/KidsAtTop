f83c6589fabec2b24fa34cd67db6ea0f5941a51c
EDUARDO <EDUARDO@DESKTOP-9H26RGO>
2020-07-18 17:30:56 -0600

    Se creo un nuevo form 2.0

--- src/kidsattop/KidsAtTop.java
+++ src/kidsattop/KidsAtTop.java
@@ -11,7 +11,7 @@ public class KidsAtTop {
     public static void main(String[] args) {
         //hola que hace
         //hola que hace Eduardo
-        //Hola que hace 2.1
+        //Hola que hace 2.2
         LoginForm formulario = new LoginForm();
         formulario.setVisible(true);
     }
