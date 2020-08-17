330a66e4b1579139bcc51d721304d04c6e99437d
Katherine Gonźalez <katy06salinas@gmail.com>
2020-08-16 18:22:55 -0600

    Reprotes de Estudiantes por Grado con y sin parametros

--- src/vistas/FrmReportes.java
+++ src/vistas/FrmReportes.java
@@ -45,6 +45,7 @@ public class FrmReportes extends javax.swing.JInternalFrame {
         jLabel1 = new javax.swing.JLabel();
         btnGenerarBitacora = new javax.swing.JButton();
         btnGenerarGradoSeccion = new javax.swing.JButton();
+        btnGenerarEstudiantesGS = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(33, 37, 41));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -72,7 +73,15 @@ public class FrmReportes extends javax.swing.JInternalFrame {
                 btnGenerarGradoSeccionActionPerformed(evt);
             }
         });
-        jPanel1.add(btnGenerarGradoSeccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 100, 220, 130));
+        jPanel1.add(btnGenerarGradoSeccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 100, 220, 130));
+
+        btnGenerarEstudiantesGS.setText("Estudiante por grado y sección ");
+        btnGenerarEstudiantesGS.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGenerarEstudiantesGSActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnGenerarEstudiantesGS, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 270, 220, 130));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 710));
 
@@ -195,9 +204,57 @@ public class FrmReportes extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_btnGenerarGradoSeccionActionPerformed
 
+    private void btnGenerarEstudiantesGSActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGenerarEstudiantesGSActionPerformed
+        int mensaje = JOptionPane.showConfirmDialog(null, "¿Deseas generar este reporte con filtros?","Información",JOptionPane.YES_NO_OPTION,JOptionPane.INFORMATION_MESSAGE);
+        if (mensaje == JOptionPane.YES_OPTION){
+             String mensaje2 = JOptionPane.showInputDialog(null, "Escribe el grado por el que quieres el reporte. Por ejemplo Kinder 5","Aviso",JOptionPane.INFORMATION_MESSAGE);
+             String mensaje3 = JOptionPane.showInputDialog(null, "Escribe la sección por el que quieres el reporte. Por ejemplo A","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                try {
+                    Conexion con = new Conexion();
+                   String reports = "RptEstudiantesPorGradoParametro";
+                   String archivo = getClass().getResource("/reportes/"+reports+".jrxml").getPath();
+
+                   archivo = URLDecoder.decode(archivo,"UTF-8");
+                   JasperReport report =
+                   JasperCompileManager.compileReport(archivo);
+                   Map<String,Object> parametros = new HashMap<String,Object>();
+                   parametros.put("grado",mensaje2); 
+                   parametros.put("seccion",mensaje3); 
+                   JasperPrint print = JasperFillManager.fillReport(report,
+                   parametros, con.conectar());
+                   JasperViewer visor = new JasperViewer(print, false);
+                   visor.setTitle("Reporte de Estudiantes por grado");
+                   visor.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                   visor.setVisible(true);
+                   reports="";
+                } 
+                catch (JRException e) {
+                    System.out.println("AQUI1");
+                    System.out.println(e.getMessage());
+                }catch (UnsupportedEncodingException ex) { 
+                    Logger.getLogger(FrmReportes.class.getName()).log(Level.SEVERE, null, ex);
+                } 
+        } else if (mensaje == JOptionPane.NO_OPTION) {
+            try{
+        
+            
+            //se crea la conexion
+            Connection cn = new Conexion().conectar();
+            JasperReport jr = JasperCompileManager.compileReport("src/reportes/RptEstudiantesPorGradoSinParametro.jrxml");
+            JasperPrint jp = JasperFillManager.fillReport(jr, null, cn);
+            JasperViewer jv = new JasperViewer(jp);
+            jv.setVisible(true);
+            
+            } catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }//GEN-LAST:event_btnGenerarEstudiantesGSActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnGenerarBitacora;
+    private javax.swing.JButton btnGenerarEstudiantesGS;
     private javax.swing.JButton btnGenerarGradoSeccion;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
