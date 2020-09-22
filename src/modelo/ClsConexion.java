f82450992389ed1de75bcf9c8da5dcb2dc010bae
EDUARDO <EDUARDO@DESKTOP-9H26RGO>
2020-09-22 00:44:55 -0600

    conexion

--- src/modelo/ClsConexion.java
+++ src/modelo/ClsConexion.java
@@ -8,8 +8,29 @@ import java.sql.DriverManager;
  * @author pablo
  */
 public class ClsConexion {
-    ///Conexion Edenilson
+    
+    private String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    private String url = "jdbc:sqlserver://2.tcp.ngrok.io:19183;databaseName=KidsAtTop";
+    private String user = "EduardoInicio";
+    private String pwrd = "eduardo2020";
+    Connection cn = null;
+    
     public Connection conectar(){
+        try
+        {
+            Class.forName(driver);
+            cn = DriverManager.getConnection(url, user, pwrd);
+            if (cn != null) {
+                System.out.println("Si hay conexion");
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return cn;
+    }
+    ///Conexion Edenilson
+    /*public Connection conectar(){
         Connection cn = null;
         try{
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -24,7 +45,7 @@ public class ClsConexion {
                     System.out.println(ex.getMessage());
             }
             return cn;
-        }
+        }*/
     
     //Conexion Kath
     /*public Connection conectar(){
